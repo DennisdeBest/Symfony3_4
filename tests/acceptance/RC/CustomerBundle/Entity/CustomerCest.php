043c780075f36337db7e8b292a2cdeb27bd35900
Dennis de Best <dennis@debest.fr>
2017-11-07 10:00:16 +0100

    Customer acceptance tests and test env setup

--- tests/acceptance/RC/CustomerBundle/Entity/CustomerCest.php
+++ tests/acceptance/RC/CustomerBundle/Entity/CustomerCest.php
@@ -1,9 +1,19 @@
 <?php
+
 namespace RC\CustomerBundle\Entity;
+
 use \AcceptanceTester;
+use JMS\Serializer\Tests\Fixtures\AccessorOrderParent;
 
 class CustomerCest
 {
+
+    private $user = [
+        'firstname' => 'John',
+        'lastname' => 'Doe',
+        'email' => 'john@doe.com'
+    ];
+
     public function _before(AcceptanceTester $I)
     {
     }
@@ -13,17 +23,26 @@ class CustomerCest
     }
 
     /**
-     * Todo delete user from database
+     * @before removeCustomerIfExists
      * @param AcceptanceTester $I
      */
     protected function createCustomerFromForm(AcceptanceTester $I)
     {
         $I->wantTo('create a customer');
-        $I->amOnPage('/app_dev.php/app/customers');
+        $this->fillCustomerForm($I);
+    }
+
+    /**
+     * Reused snippet to fill in the customer form
+     * @param AcceptanceTester $I
+     */
+    protected function fillCustomerForm(AcceptanceTester $I)
+    {
+        $I->amOnPage('/app_test.php/app/customers');
         $I->click('Create');
-        $I->fillField('Firstname', 'john');
-        $I->fillField('Lastname', 'doe');
-        $I->fillField('Email', 'john@doe.com');
+        $I->fillField('Firstname', $this->user['firstname']);
+        $I->fillField('Lastname', $this->user['lastname']);
+        $I->fillField('Email', $this->user['email']);
         $I->click('Create');
     }
 
@@ -33,10 +52,28 @@ class CustomerCest
      */
     public function checkThatCreatedUserIsInDatabase(AcceptanceTester $I)
     {
-        $I->seeInRepository('RCCustomerBundle:Customer', ['email' => 'john@doe.com']);
+        $I->seeInRepository('RCCustomerBundle:Customer', ['email' => $this->user['email']]);
+        $I->canSeeInCurrentUrl('/app_test.php/app/customers');
     }
 
-    protected function removeCustomerIfExists(AcceptanceTester $I){
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function createSameUserTwiceShouldFail(AcceptanceTester $I)
+    {
+        $I->wantTo('create a customer with an email that is already used');
+        $this->fillCustomerForm($I);
+        $I->canSeeInCurrentUrl('/app_test.php/app/customers/new');
+    }
 
+    /**
+     * Delete the customer before retesting
+     * @param AcceptanceTester $I
+     */
+    protected function removeCustomerIfExists(AcceptanceTester $I)
+    {
+        $I->wantTo('delete a customer');
+        $I->amOnPage('/app_test.php/app/customers');
+        $I->click('Delete');
     }
 }
